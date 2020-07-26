823e486849969402804a62fe2f3714fbe8eac2da
Artak11111 <Artak11111@users.noreply.github.com>
2020-07-26 22:31:23 +0400

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,27 @@
-window.onscroll = function() {scrollFunction()};
+window.onload = function(){
+	var index = 1;
 
-function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("navbar-container").className = "navbar-container-light";
-    document.getElementById("navbar").className = "navbar-light";
-  } else {
-    document.getElementById("navbar-container").className = "navbar-container-dark";
-    document.getElementById("navbar").className = "navbar-dark";
-  }
+	setInterval(function(){
+		if(index === 1)
+			document.getElementById("carousel").style.marginLeft = "-100vw";
+		else if (index === 2)
+			document.getElementById("carousel").style.marginLeft = "-200vw";
+		else {
+			document.getElementById("carousel").style.marginLeft = "0";
+			index = 0;
+		}
+
+		index++;
+	},3000);
 }
+
+window.onscroll = function() 
+{
+	if (document.body.scrollTop > 95 || document.documentElement.scrollTop > 95) {
+    	document.getElementById("navbar-container").className = "navbar-container-light";
+    	document.getElementById("navbar").className = "navbar-light";
+	} else {
+	    document.getElementById("navbar-container").className = "navbar-container-dark";
+	    document.getElementById("navbar").className = "navbar-dark";
+	}
+};
