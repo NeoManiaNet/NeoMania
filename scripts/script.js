bb89ba843efa3f1e7cf2c4288a557f70a4696b90
Artak11111 <Artak11111@users.noreply.github.com>
2020-08-06 15:52:46 +0400

    Added about part of page

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,11 @@
 var app = {
+	hasAboutLoaded: false,
 	slideIndex: 0,
 	slideInterval: null,
-	slide: function (toRight){
+	slide: function (toRight, fromButton){
+			if(fromButton === true)
+			this.resetInterval();
+
 			if(toRight === true)
 				app.slideIndex++;
 			else
@@ -21,11 +25,11 @@ var app = {
 		}
 	},
 	resetInterval: function ResetInterval(){
-			clearInterval(app.slide);
+			clearInterval(this.slideInterval);
 
-			setInterval(function(){
-				app.slide(true);
-			},15000);
+			this.slideInterval = setInterval(function(){
+				app.slide(true, false);
+			},7000);
 	}
 };
 
@@ -34,15 +38,36 @@ window.onload = function(){
 	app.resetInterval();
 }
 
+/* scrolling function */
+
 window.onscroll = function() 
 {
-	if (document.body.scrollTop > 98 || document.documentElement.scrollTop > 98) {
+	var scrollTop = document.documentElement.scrollTop;
+
+	if (scrollTop> 50) {
     	document.getElementById("navbar-container").className = "navbar-container-light";
     	document.getElementById("navbar").className = "navbar-light";
 	} else {
 	    document.getElementById("navbar-container").className = "navbar-container-dark";
 	    document.getElementById("navbar").className = "navbar-dark";
 	}
-};
 
-/* functions */
+	if(scrollTop > 300 && !app.hasAboutLoaded)
+	{
+		app.hasAboutLoaded = true;
+
+		document.getElementById("who-we-are").style.marginTop = "5vh";
+		document.getElementById("who-we-are").style.opacity = "1";
+
+		document.getElementById("what-we-do").style.marginTop = "39vh";
+		document.getElementById("what-we-do").style.opacity = "1";
+
+		document.getElementById("what-we-do-first-part").style.marginTop = "0vh";
+		document.getElementById("what-we-do-first-part").style.opacity = "1";
+
+		document.getElementById("what-we-do-second-part").style.marginTop = "0vh";
+		document.getElementById("what-we-do-second-part").style.opacity = "1";
+	}
+
+	console.log(document.body.scrollTop + " " + document.documentElement.scrollTop)
+};
