b4346e8a05de842ac7bd2bc6a265fab21df64292
Artak Galstyan <4473149@gmail.com>
2020-08-16 00:16:21 +0400

    Solution page

---
+++ scripts/index.script.js
@@ -0,0 +1,60 @@
+var page = {
+	hasAboutLoaded: false,
+	slideIndex: 0,
+	slideInterval: null,
+	slide: function (toRight, fromButton){
+			if(fromButton === true)
+			this.resetInterval();
+
+			if(toRight === true)
+				this.slideIndex++;
+			else
+                this.slideIndex--;
+
+			if(this.slideIndex === 3)
+                this.slideIndex = 0;
+			else if (this.slideIndex === -1)
+                this.slideIndex = 2;
+
+			if(this.slideIndex === 1)
+				document.getElementById("carousel").style.marginLeft = "-100vw";
+			else if (this.slideIndex === 2)
+				document.getElementById("carousel").style.marginLeft = "-200vw";
+			else {
+				document.getElementById("carousel").style.marginLeft = "0";
+		}
+	},
+	resetInterval: function ResetInterval(){
+			clearInterval(this.slideInterval);
+
+			this.slideInterval = setInterval(function(){
+				page.slide(true, false);
+			}, 7000);
+	}
+};
+
+window.onload = function(){
+	page.resetInterval();
+}
+
+window.addEventListener("scroll", function() 
+{
+	var scrollTop = document.documentElement.scrollTop;
+
+	if(scrollTop > 300 && !page.hasAboutLoaded)
+	{
+		page.hasAboutLoaded = true;
+
+		document.getElementById("who-we-are").style.marginTop = "10vh";
+		document.getElementById("who-we-are").style.opacity = "1";
+
+		document.getElementById("what-we-do").style.marginTop = "44vh";
+		document.getElementById("what-we-do").style.opacity = "1";
+
+		document.getElementById("what-we-do-first-part").style.marginTop = "2vh";
+		document.getElementById("what-we-do-first-part").style.opacity = "1";
+
+		document.getElementById("what-we-do-second-part").style.marginTop = "2vh";
+		document.getElementById("what-we-do-second-part").style.opacity = "1";
+	}
+});
--- scripts/script.js
+++ scripts/script.js
@@ -1,49 +1,12 @@
 var app = {
-	hasAboutLoaded: false,
-	slideIndex: 0,
-	slideInterval: null,
-	slide: function (toRight, fromButton){
-			if(fromButton === true)
-			this.resetInterval();
-
-			if(toRight === true)
-				app.slideIndex++;
-			else
-				app.slideIndex--;
-
-			if(app.slideIndex === 3)
-				app.slideIndex = 0;
-			else if (app.slideIndex === -1)
-				app.slideIndex = 2;
-
-			if(app.slideIndex === 1)
-				document.getElementById("carousel").style.marginLeft = "-100vw";
-			else if (app.slideIndex === 2)
-				document.getElementById("carousel").style.marginLeft = "-200vw";
-			else {
-				document.getElementById("carousel").style.marginLeft = "0";
-		}
-	},
-	resetInterval: function ResetInterval(){
-			clearInterval(this.slideInterval);
-
-			this.slideInterval = setInterval(function(){
-				app.slide(true, false);
-			},7000);
-	},
 	scrollToBottom: function(){
-		window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
+		window.scrollTo({ top: document.body.scrollHeight - 1000, behavior: 'smooth' });
 	}
 };
 
-
-window.onload = function(){
-	app.resetInterval();
-}
-
 /* scrolling function */
 
-window.onscroll = function() 
+window.addEventListener("scroll", function() 
 {
 	var scrollTop = document.documentElement.scrollTop;
 
@@ -54,23 +17,4 @@ window.onscroll = function()
 	    document.getElementById("navbar-container").className = "navbar-container-dark";
 	    document.getElementById("navbar").className = "navbar-dark";
 	}
-
-	if(scrollTop > 300 && !app.hasAboutLoaded)
-	{
-		app.hasAboutLoaded = true;
-
-		document.getElementById("who-we-are").style.marginTop = "10vh";
-		document.getElementById("who-we-are").style.opacity = "1";
-
-		document.getElementById("what-we-do").style.marginTop = "44vh";
-		document.getElementById("what-we-do").style.opacity = "1";
-
-		document.getElementById("what-we-do-first-part").style.marginTop = "2vh";
-		document.getElementById("what-we-do-first-part").style.opacity = "1";
-
-		document.getElementById("what-we-do-second-part").style.marginTop = "2vh";
-		document.getElementById("what-we-do-second-part").style.opacity = "1";
-	}
-
-	console.log(document.body.scrollTop + " " + document.documentElement.scrollTop)
-};
+});
