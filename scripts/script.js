2528d53a6b827a89a29dbf86773e3a9e7a19c203
Artak Galstyan <4473149@gmail.com>
2020-09-05 22:09:08 +0400

    test

--- scripts/script.js
+++ scripts/script.js
@@ -18,13 +18,35 @@ var app = {
 		xmlHttp.open( "POST", "https://localhost:44337/Applications/Create", false ); // false for synchronous request
 		xmlHttp.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
 		xmlHttp.setRequestHeader('Access-Control-Allow-Origin', 'https://localhost:44337/');
+
+		xmlHttp.onreadystatechange = function() {
+			if (xmlHttp.readyState === 4) {
+				var response = xmlHttp.responseText;
+				console.log(response);
+		
+				if(response === "true")
+				{
+					Swal.fire(
+						"Thank you for your application!" ,
+						"We\'ll contact you soon.",
+						'success'
+					  );
+				}else{
+					Swal.fire({
+						icon: 'error',
+						title: 'You got someting wrong',
+						text: 'Something went wrong!'
+					  })
+				}
+			}
+		  }
+
 		xmlHttp.send( JSON.stringify({
 			Name:document.getElementById("name").value,
 			EMail:document.getElementById("email").value,
 			PhoneNumber:document.getElementById("phone").value,
 			AdditionalMessage : document.getElementById("msg").value
 		}));
-		return xmlHttp.responseText;
 	}
 };
 
